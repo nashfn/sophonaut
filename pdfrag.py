4d3f6005444d9709f1d55188e9df23a04585a4f5
Arunesh Mishra <aruneshm@gmail.com>
2024-10-10 11:20:46 -0700

    gitignore

--- pdfrag.py
+++ pdfrag.py
@@ -7,3 +7,8 @@ from llama_index.core import Settings
 from llama_parse import LlamaParse
 
 documents = LlamaParse(result_type="markdown").load_data("./apple_2021_10k.pdf")
+
+
+
+if __name__ == "__main__":
+    run_main()
